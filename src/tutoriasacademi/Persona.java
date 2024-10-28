b5278169d1b9426cbd98e1c30f59caca895062a1
yemm04 <yoshitomochizaki0@gmail.com>
2024-10-28 20:57:25 +0100

    persona clase

--- src/tutoriasacademi/Persona.java
+++ src/tutoriasacademi/Persona.java
@@ -1,13 +1,83 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package tutoriasacademi;
 
-/**
- *
- * @author jeffersonparedespretel
- */
-public class Persona {
+
+
+
+abstract class Persona {
+    private int codigo;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String fechaDeNacimiento;
+    private String nacionalidad;
     
+    
+    
+    
+    public Persona(int codigo, String nombre, String apellido, String correo, String fechaDeNacimiento, String nacionalidad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+
+
+
+    // Método abstracto
+    public abstract void mostrarInformacion();
+    // Aca escribimos
+    public static void main(String[] args) {
+
+     
+        
+    }
 }
