892e6e37b7dec29848c8968d0f36e362fe4eb3d9
yemm04 <yoshitomochizaki0@gmail.com>
2024-10-28 18:44:34 +0100

    administracion clase

---
+++ src/tutoriasacademi/Administracion.java
@@ -0,0 +1,45 @@
+
+package tutoriasacademi;
+
+
+public class Administracion extends Persona {
+    private String fechaGestion;
+    private String descripcion;
+    private String tipoGestion;
+
+    public Administracion(int codigo, String nombre,int edad, String fechaGestion, String descripcion, String tipoGestion) {
+        super(codigo, nombre, edad);
+        this.fechaGestion = fechaGestion;
+        this.descripcion = descripcion;
+        this.tipoGestion = tipoGestion;
+    }
+
+    public String getFechaGestion() {
+        return fechaGestion;
+    }
+
+    public void setFechaGestion(String fechaGestion) {
+        this.fechaGestion = fechaGestion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipoGestion() {
+        return tipoGestion;
+    }
+
+    public void setTipoGestion(String tipoGestion) {
+        this.tipoGestion = tipoGestion;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Administración: " + getNombre() + ", Código: " + getCodigo() + ", edad : " + getEdad()+ ", Fecha de Gestión: " + fechaGestion + ", Descripción: " + descripcion + ", Tipo de Gestión: " + tipoGestion);
+    }
+}
