7abf80c8f1c3262c598ba2e785cdb295e41c2f99
yemm04 <yoshitomochizaki0@gmail.com>
2024-10-28 20:57:38 +0100

    profesor clase

--- src/tutoriasacademi/Profesor.java
+++ src/tutoriasacademi/Profesor.java
@@ -1,13 +1,81 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package tutoriasacademi;
 
-/**
- *
- * @author alexr
- */
-public class Profesor {
+public class Profesor extends Persona{
+     
+    private String Especialidad;
+    private String Descripcion;
+    private double CostoPorHora;
+    private String MaterialEstudio;
+    private String Disponibilidad;
+    private String Reportar;     
     
+    public Profesor(int codigo, String nombre, String apellido, String correo, String fechaDeNacimiento, String nacionalidad, String Especialidad, String Descripcion, double CostoPorHora, String MaterialEstudio, String Disponibilidad, String Reportar) {
+        super(codigo, nombre, apellido, correo, fechaDeNacimiento, nacionalidad);
+       
+        this.Especialidad = Especialidad;
+        this.Descripcion = Descripcion;
+        this.CostoPorHora = CostoPorHora;
+        this.MaterialEstudio = MaterialEstudio;
+        this.Disponibilidad = Disponibilidad;
+        this.Reportar = Reportar;
+              
+    }
+
+    public String getEspecialidad() {
+        return Especialidad;
+    }
+
+    public void setEspecialidad(String Especialidad) {
+        this.Especialidad = Especialidad;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public double getCostoPorHora() {
+        return CostoPorHora;
+    }
+
+    public void setCostoPorHora(double CostoPorHora) {
+        this.CostoPorHora = CostoPorHora;
+    }
+
+    public String getMaterialEstudio() {
+        return MaterialEstudio;
+    }
+
+    public void setMaterialEstudio(String MaterialEstudio) {
+        this.MaterialEstudio = MaterialEstudio;
+    }
+
+    public String getDisponibilidad() {
+        return Disponibilidad;
+    }
+
+    public void setDisponibilidad(String Disponibilidad) {
+        this.Disponibilidad = Disponibilidad;
+    }
+
+    public String getReportar() {
+        return Reportar;
+    }
+
+    public void setReportar(String Reportar) {
+        this.Reportar = Reportar;
+    }
+
+    
+
+
+                                                                                                                                                                                                                                                                
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Codigo: " + getCodigo() + ", Nombre: " + getNombre() + ", Apellido: " + getApellido() + ", Correo: " + getCorreo() +", fechaDeNacimiento : " + getFechaDeNacimiento()+ ", nacionalidad : " + getNacionalidad() + ", Especialidad: " + Especialidad + ", Descripcion :  " + Descripcion + ", Costo Por Hora : " + CostoPorHora + ", Material Estudio : " + MaterialEstudio + ", Disponibilidad : " + Disponibilidad +", Reportar : " + Reportar);
+    }
 }
