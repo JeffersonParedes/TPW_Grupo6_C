4713411b715e0e1ced236c8a2fa679b953d9d54f
yemm04 <yoshitomochizaki0@gmail.com>
2024-10-29 22:50:07 +0100

    disponibilidad clase

--- src/tutoriasacademi/Disponibilidad.java
+++ src/tutoriasacademi/Disponibilidad.java
@@ -1,13 +1,73 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package tutoriasacademi;
 
-/**
- *
- * @author Valeria
- */
+
 public class Disponibilidad {
     
+    private int año;
+    private String mes;
+    private int dia;
+    private double horaInicio;
+    private double horaFin;
+    
+    public Disponibilidad (int año, String mes, int dia, double horaInicio, double horaFin){
+   this.año = año;
+   this.mes = mes;
+   this.dia = dia;
+   this.horaInicio = horaInicio;
+   this.horaFin = horaFin;
+        
+    
 }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public double getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(double horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public double getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(double horaFin) {
+        this.horaFin = horaFin;
+    }
+    
+    public void mostrarDisponibilidad() { 
+        System.out.println("Disponibilidad:"); 
+        System.out.println("Año: " + año); 
+        System.out.println("Mes: " + mes); 
+        System.out.println("Día: " + dia); 
+        System.out.println("Hora de inicio: " + horaInicio); 
+        System.out.println("Hora de fin: " + horaFin);
+    
+    
+}
+    }
