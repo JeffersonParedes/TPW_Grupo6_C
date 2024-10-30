b10a42ce039239248930fb1d299ac2b33614760f
yemm04 <yoshitomochizaki0@gmail.com>
2024-10-30 08:31:43 +0100

    alumnoclaseff

--- src/ArraysList/AlumnoArrayList.java
+++ src/ArraysList/AlumnoArrayList.java
@@ -1,7 +1,30 @@
-
 package ArraysList;
 
+import java.util.ArrayList;
+import java.util.List;
+import tutoriasacademi.Alumno;
 
 public class AlumnoArrayList {
     
+    List<Alumno> listaAlumnos = new ArrayList<>();
+
+    // Método para agregar un alumno a la lista
+    public void agregar(Alumno a) {
+        listaAlumnos.add(a);
+    }
+
+    // Método para quitar un alumno de la lista
+    public void quitar(Alumno a) {
+        listaAlumnos.remove(a);
+    }
+
+    // Método para obtener un alumno por índice
+    public Alumno obtener(int pos) {
+        return listaAlumnos.get(pos);
+    }
+
+    // Método para listar todos los alumnos
+    public List<Alumno> listar() {
+        return this.listaAlumnos;
+    }
 }
