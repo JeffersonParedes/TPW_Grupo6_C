9148a8f965514d0b3150c3b98540c6fdba3a9af8
JeffersonParedes <valentinopp.2005@gmail.com>
2024-11-03 18:30:07 -0500

    Reestructuracion del Proyecto para las partes de mis compañeros

--- src/tutoriasacademi/TutoriasAcademi.java
+++ src/tutoriasacademi/TutoriasAcademi.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package tutoriasacademi;
 
-/**
- *
- * @author jeffersonparedespretel
- */
+import tutoriasacademi.IGU.Registro;
+
+
 public class TutoriasAcademi {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Registro regis = new Registro();
+        regis.setVisible(true);
+        regis.setLocationRelativeTo(null);
     }
     
 }
